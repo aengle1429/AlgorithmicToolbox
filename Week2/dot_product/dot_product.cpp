a45c6da0ab6fe247475c0991fe416753f5fc4a0f
Abe <aengle1429@gmail.com>
2017-08-07 12:25:28 -0700

    Add Week 2 Exercise 3.

--- Week2/dot_product/dot_product.cpp
+++ Week2/dot_product/dot_product.cpp
@@ -6,11 +6,44 @@ using std::vector;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
-  long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
-  }
-  return result;
+    if(a.size() > 1)
+    {
+        for(int i = 1; i < a.size(); ++i)
+        {
+            int key1 = a.at(i);
+            int key2 = b.at(i);
+            int j = i - 1;
+            int k = i - 1;
+
+            while(k >= 0 && b.at(k) > key2)
+            {
+                b.at(k+1) = b.at(k);
+                k--;
+            }
+            b.at(k+1) = key2;
+
+
+            while(j >= 0 && a.at(j) > key1)
+            {
+                a.at(j+1) = a.at(j);
+                j--;
+            }
+            a.at(j+1) = key1;
+        }
+    }
+    // for(int i = 0; i < a.size(); ++i)
+    // {
+    //     std::cout << a.at(i) << " ";
+    // }
+    // for(int i = 0; i < a.size(); ++i)
+    // {
+    //     std::cout << b.at(i) << " ";
+    // }
+    long long result = 0;
+    for (size_t i = 0; i < a.size(); i++) {
+      result += ((long long) a[i]) * b[i];
+    }
+    return result;
 }
 
 int main() {
