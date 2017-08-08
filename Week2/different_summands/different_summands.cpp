b4bbf57691be2dd8c04bbb0d582f2bd0972cc168
Abe Engle <aengle1429@gmail.com>
2017-08-08 12:48:42 -0700

    Add all week 2 solutions.

--- Week2/different_summands/different_summands.cpp
+++ Week2/different_summands/different_summands.cpp
@@ -3,9 +3,36 @@
 
 using std::vector;
 
+// Given a positive integer n, find the maximum number of distinct positive summands that total n.
+// There are multiple optima; e.g., 8 = 1 + 2 + 5 = 1 + 3 + 4.
+// The greedy approach
+
+int summands_helper(int l, int k)
+{
+	if(k <= 2*l)
+	{
+		return k;
+	}
+	else
+	{
+		return l;
+	}
+}
+
 vector<int> optimal_summands(int n) {
   vector<int> summands;
   //write your code here
+  int l = 1;
+  int k = n;
+  while(k > 0)
+  {
+    int ans = summands_helper(l, k);	
+	summands.push_back(ans);
+	// std::cout << ans << std::endl;
+	k = k - ans;
+	l = l + 1;
+    // std::cout << l << " " << k << "\n";
+  }
   return summands;
 }
 
--- Week2/largest_number/largest_number.cpp
+++ Week2/largest_number/largest_number.cpp
@@ -10,9 +10,27 @@ using std::string;
 string largest_number(vector<string> a) {
   //write your code here
   std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
+  while(a.size() > 0)
+  {
+	  int maxdigit = 0;
+	  int maxlocation = -1;
+	  for(int i = 0; i < a.size(); ++i)
+	  {
+		// if(std::stoi(a.at(i)) >= maxdigit)
+		// string smaxdigit = std::to_string(maxdigit);
+		// std::cout << a.at(i) + std::to_string(maxdigit) << "\n";
+		if(std::stoi(a.at(i) + std::to_string(maxdigit)) >= std::stoi(std::to_string(maxdigit) + a.at(i)))
+		{
+			maxdigit = std::stoi(a.at(i));
+			maxlocation = i;
+		}
+	  }
+	  ret << a.at(maxlocation);
+	  a.erase(a.begin() + maxlocation);
   }
+  // for (size_t i = 0; i < a.size(); i++) {
+  //   ret << a[i];
+  // }
   string result;
   ret >> result;
   return result;
