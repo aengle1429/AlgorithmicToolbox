cbf91f7d12d8e62d8a0639ff1e058f1601d21228
Abe Engle <aengle1429@gmail.com>
2017-08-11 15:53:33 -0700

    Add Sedgewick mergesort counting inversions.

--- Week3/inversions/inversions.cpp
+++ Week3/inversions/inversions.cpp
@@ -3,53 +3,74 @@
 
 using std::vector;
 
-void mergeAB(vector<int> &c, vector<int> &a, int N, vector<int> &b, int M)
+// void mergeAB(int c[], int a[], int N, int b[], int M)
+// {
+//     for(int i = 0, j = 0, k = 0; k < N + M; ++k)
+//     {
+// 
+//         if(i==N) { c[k] = b[j++]; continue;}
+//         if(j==M) { c[k] = a[i++]; continue;}
+//         c[k] = (a[i] < b[j]) ? a[i++] : b[j++];
+//     }
+// }
+// 
+long long mergeAB(long long c[], long long a[], int N, long long b[], int M)
 {
+    long long numInv = 0;
     for(int i = 0, j = 0, k = 0; k < N + M; ++k)
     {
 
         if(i==N) { c[k] = b[j++]; continue;}
         if(j==M) { c[k] = a[i++]; continue;}
-        c[k] = (a[i] < b[j]) ? a[i++] : b[j++];
+        if(a[i] <= b[j]) c[k] = a[i++];
+        else{numInv+=(N-i); c[k] = b[j++];}
     }
+    return numInv;
 }
 
-void mergesortABr(vector<int> &a, vector<int> &b, int l, int r)
+long long mergesortABr(long long a[],long long b[], int l, int r)
 {
-    if(r <= l) { return; }
+    long long numInv = 0;
+    //recursion set up to sort subcomponents of b[l,...,r] and merge them into a[l,...,r].
+    if(r <= l){ return numInv; }
     int m = (l + r) / 2;
-    mergesortABr(b, a, l, m);
-    mergesortABr(b, a, m+1, r);
-    merge(a+l, b+l, m-l+1, b+m+1, r-m);
+    long long i1 = mergesortABr(b, a, l, m);
+    long long i2 = mergesortABr(b, a, m+1, r);
+    // merge the files from b into a.
+    // all data movement is done during merge
+    numInv += mergeAB(&a[l], &b[l], m-l+1, &b[m+1], r-m);
+    return i1 + i2 + numInv;
 }
 
-void mergesortAB(vector<int> &a, int l, int r)
+void mergesortAB(long long a[], int l, int r)
 {
-    vector<int> &aux(r-l+1);
-    for(int i = l; i <= r; ++i) aux[i] = a[i];
-    mergesortABr(a, aux, l, r);
+    long long aux [r-l+1];
+    for(int i = 0; i <= r-l; ++i) aux[i] = a[i];
+    long long inversions = mergesortABr(a, aux, 0, r-l);
+    // for(int i = 0; i <= r-l; ++i) std::cout<<a[i]<<std::endl;
+    std::cout << inversions << std::endl;
 }
 
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
-    long long number_of_inversions = 0;
-    if (right <= left + 1)
-    {
-        return number_of_inversions;
-    }
-    size_t ave = left + (right - left) / 2;
-    number_of_inversions += get_number_of_inversions(a, b, left, ave);
-    number_of_inversions += get_number_of_inversions(a, b, ave, right);
-    return number_of_inversions;
-}
-
+// long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
+//     long long number_of_inversions = 0;
+//     if (right <= left + 1)
+//     {
+//         return number_of_inversions;
+//     }
+//     size_t ave = left + (right - left) / 2;
+//     number_of_inversions += get_number_of_inversions(a, b, left, ave);
+//     number_of_inversions += get_number_of_inversions(a, b, ave, right);
+//     return number_of_inversions;
+// }
+// 
 int main() {
     int n;
     std::cin >> n;
-    vector<int> a(n);
-    for (size_t i = 0; i < a.size(); i++) {
+    long long a [n];
+    for (size_t i = 0; i < n; i++) {
       std::cin >> a[i];
     }
-    vector<int> b(a.size());
-    std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+    mergesortAB(a, 0, n-1);
+    //std::cout << get_number_of_inversions(a, b, 0, n) << '\n';
 }
