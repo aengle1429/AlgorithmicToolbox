5b50d3e38f4942730f8a60f14bac2347b097db08
Abe <aengle1429@gmail.com>
2017-08-18 15:44:33 -0700

    Add solutions for Week 4 Ex 1 and 2.

--- Week4/knapsack/knapsack.cpp
+++ Week4/knapsack/knapsack.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
+#include <array>
 #include <vector>
-
+using std::array;
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+/* Idea : Let val(w, i) denote the optimal value of a knapsack
+ * with size w using the first i items, letting val(0, i) = val(w, 0) = 0.
+ * Then the optimal knapsack at (w, i) either contains item i or not.
+ * In the former, val(w - w_i, i - 1) is an optimal knapsack
+ * which we can add w_i to to obtain (w, i). In the latter,
+ * val(w, i) = val(w, i - 1) since item i is not present. 
+ * Therefore val(w, i) = max( val(w - w_i, i - 1), val(w, i - 1) )
+ * And obviously w - w_i >=0 is a necessary condition for the max.
+ */
+int optimal_weight(int W, const vector<int> &items)
+{
+    int n = items.size();
+    // int matrix [W+1][n+1];
+    vector<array<int, 301>> matrix;
+    //write your code here
+    for(int w = 0; w < W + 1; ++w)
+    {
+        array<int, 301> arr;
+        arr[0] = 0;
+        matrix.push_back(arr);
     }
-  }
-  return current_weight;
+
+    for(int i = 0; i < n + 1; ++i)
+    {
+        matrix[0][i] = 0;
+    }
+
+
+    for(int w = 1; w < W + 1; ++w)
+    {
+        for(int i = 1; i < n + 1; ++i)
+        {
+            int val = matrix[w][i-1];
+            if( w - items[i - 1] >= 0 )
+            {
+                // std::cout << "Here is items[i] " << items[i] << std::endl;
+                int val2 = matrix[w - items[i - 1]][i - 1] + items[i - 1];
+                matrix[w][i] = val > val2 ? val : val2;
+            }
+            else{
+                matrix[w][i] = val;
+            }
+        }
+    }
+
+    // int current_weight = 0;
+    // for (size_t i = 0; i < items.size(); ++i) {
+    //     if (current_weight + items[i] <= W) {
+    //         current_weight += items[i];
+    //     }
+    // }
+    return matrix[W][n];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int n, W;
+    std::cin >> W >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> w[i];
+    }
+    std::cout << optimal_weight(W, w) << '\n';
 }
--- Week4/primitive_calculator/primitive_calculator.cpp
+++ Week4/primitive_calculator/primitive_calculator.cpp
@@ -1,31 +1,121 @@
+#include <limits>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using std::vector;
+using namespace std;
 
-vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
-    }
+// the greedy algorithm here fails with n = 10
+// greedy proposes the sequence 10 5 4 2 1
+// optimal is 10 9 3 1
+
+/*
+ *steps to dynamic programming are
+ *1. Define subproblems
+ *2. Guess part of solution
+ *3. Recurrence (time / subproblem)
+ *4. Recurse + Memoize (acyclic) or Bottom-up (topo ordering needed)
+ * time = #subproblem  * time/subproblem
+ *5. Original problem
+*/
+
+
+// Here we try Recurse + Memoize. However, for n large enough the recursion tree is too deep, so stack overflow.
+
+int optimal_sequence(int n, vector<int> &memo, vector<int> &backtrack) {
+  if(n > 1 && memo.at(n - 1) >= 1)
+  {
+      return memo.at(n - 1);
   }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+  if(n == 1)
+  {
+      return n - 1;
+  }
+  else
+  {
+      int p1, p2, p3;
+      p3 = optimal_sequence(n - 1, memo, backtrack);
+      p2 = n % 2 == 0 ? optimal_sequence(n / 2, memo, backtrack) : numeric_limits<int>::max(); 
+      p1 = n % 3 == 0 ? optimal_sequence(n / 3, memo, backtrack) : numeric_limits<int>::max();
+      // std::cout << "The numbers for n= " << n << " are " << p1 << " " << p2 << " " << p3 << endl;
+      int ans = min({p1, p2, p3}) + 1;
+      memo[n-1] = ans;
+      if( p1 <= p2 )
+      {
+       if( p1 <= p3)
+       {
+         backtrack.at(n - 1) = n / 3;
+       }
+       else
+       {
+         backtrack.at(n - 1) = n - 1;
+       }
+      }
+      else
+      {
+       if( p2 <= p3 )
+       {
+         backtrack.at(n - 1) = n / 2;
+       }
+       else
+       {
+         backtrack.at(n - 1) = n - 1;
+       }
+      }
+      return ans;
+   }
+}
+
+void optimal_sequence2(int n, vector<int> &memo, vector<int> &backtrack)
+{
+    // Place ith answer in memo[i - 1]
+    memo.at(0) = 0;
+    for(int i = 2; i <= n; ++i)
+    {
+        int p1 = i % 3 == 0 ? memo.at(i / 3 - 1) : numeric_limits<int>::max();
+        int p2 = i % 2 == 0 ? memo.at(i / 2 - 1) : numeric_limits<int>::max();
+        int p3 = memo.at(i - 2);
+        int ans = min({p1, p2, p3}) + 1;
+        memo.at(i-1) = ans;
+        if( p1 <= p2 )
+        {
+        if( p1 <= p3)
+        {
+        backtrack.at(i - 1) = i / 3;
+        }
+        else
+        {
+        backtrack.at(i - 1) = i - 1;
+        }
+        }
+        else
+        {
+        if( p2 <= p3 )
+        {
+         backtrack.at(i - 1) = i / 2;
+        }
+        else
+        {
+         backtrack.at(i - 1) = i - 1;
+        }
+        }
+    }          
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
+  vector<int> sequence(n, 0), backtrack(n, 0);
+  optimal_sequence2(n, sequence, backtrack);
+  cout << sequence.back() << endl;
+  vector<int> answ;
+  answ.push_back(n);
+  while(n > 1)
+  {
+      answ.push_back(backtrack[n - 1]); 
+      n = backtrack[n - 1];
   }
+  reverse(answ.begin(), answ.end());
+  for(int i = 0; i < answ.size(); ++i) std::cout << answ[i] << " ";
 }
